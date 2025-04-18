b00c50459cd40d1245aac8c297a377f8e94cab3a
YUYueke-003 <yuyueke@connect.hku.hk>
2025-04-18 22:01:26 +0800

    uni-app front end version 1

---
+++ pages/list/list.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<button class="button" size="mini" type="primary" plain="true" loading="true"> confirm </button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+</style>
